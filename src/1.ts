b8c8955a9cc52cfe18f913d1d1f12b7c786c48c0
farhanarnob44 <farhanarnob44@gmail.com>
2025-05-08 21:57:15 +0600

    number 5 is done

--- src/1.ts
+++ src/1.ts
@@ -50,5 +50,12 @@
 
 }
 {
-    
+    function processValue(value: string | number): number{
+        if(typeof value === "string" ){
+            return value.length
+        }
+        else{
+            return value*2;
+        }
+    }
 }
